20cd670f0e9ec70c26fb493c66ff8583af1cfd08
Krystyna Ewing <krystyna.d.l.ewing@gmail.com>
2021-09-14 15:45:09 -0400

    Set up main and virtual pet classes

---
+++ src/main/java/virtual_pet/VirtualPet.java
@@ -0,0 +1,35 @@
+package virtual_pet;
+
+public class VirtualPet {
+    private String name;
+    private int hungerLevel;
+    private int thirstLevel;
+    private int boredomLevel;
+
+    public VirtualPet(String name, int hungerLevel, int thirstLevel, int boredomLevel){
+        this.name = name;
+        this.hungerLevel = hungerLevel;
+        this.thirstLevel = thirstLevel;
+        this.boredomLevel = boredomLevel;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHungerLevel() {
+        return hungerLevel;
+    }
+
+    public int getThirstLevel() {
+        return thirstLevel;
+    }
+
+    public int getBoredomLevel() {
+        return boredomLevel;
+    }
+
+
+
+}
--- src/main/java/virtual_pet/VirtualPetApplication.java
+++ src/main/java/virtual_pet/VirtualPetApplication.java
@@ -2,8 +2,11 @@ package virtual_pet;
 
 public class VirtualPetApplication {
 
+    static VirtualPet llama = new VirtualPet("Llama charlie", 50,50,50);
+
     public static void main(String[] args) {
-        //Interact with a VirtualPet object in this method
+        System.out.println("Welcome to the Virtual Pet Farm! I'm " + llama.getName() + " !");
+        System.out.println(" You can feed me, give me water, and check my boredom level! But beware! If I get bored, I spit! ");
     }
 
 }
