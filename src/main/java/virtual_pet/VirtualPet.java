5dbf8f757a488af395620cb9c9146921fed98175
Krystyna Ewing <krystyna.d.l.ewing@gmail.com>
2021-09-15 14:54:47 -0400

    started first method

--- src/main/java/virtual_pet/VirtualPet.java
+++ src/main/java/virtual_pet/VirtualPet.java
@@ -1,4 +1,8 @@
 package virtual_pet;
+import java.util.Scanner;
+
+
+
 
 public class VirtualPet {
     private String name;
@@ -19,17 +23,38 @@ public class VirtualPet {
     }
 
     public int getHungerLevel() {
+
         return hungerLevel;
     }
 
     public int getThirstLevel() {
+
         return thirstLevel;
     }
 
     public int getBoredomLevel() {
+
         return boredomLevel;
     }
 
+  public void gettingStarted() {
+        Scanner input =  new Scanner(System.in);
+        String inputNum;
 
+        System.out.println("To get started with my care, what would you like to check first? ");
+        System.out.println("Please enter 1 to check my hunger level");
+        System.out.println("Please enter 2 to check my thirst level");
+        System.out.println("Please enter 3 to check my boredom level");
+        inputNum = input.nextLine();
+
+        if (Integer.parseInt(inputNum) == 1){
+            System.out.println("My hunger level is "  + getHungerLevel());
+        } else if (Integer.parseInt(inputNum) == 2) {
+            System.out.println("My thirst level is "  + getThirstLevel());
+        }else if (Integer.parseInt(inputNum) == 3) {
+            System.out.println("My thirst level is "  + getBoredomLevel());
+        }
+
+  }
 
 }
--- src/main/java/virtual_pet/VirtualPetApplication.java
+++ src/main/java/virtual_pet/VirtualPetApplication.java
@@ -2,11 +2,12 @@ package virtual_pet;
 
 public class VirtualPetApplication {
 
-    static VirtualPet llama = new VirtualPet("Llama charlie", 50,50,50);
+    static VirtualPet llama = new VirtualPet("Llama Charlie", 50,50,50);
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Virtual Pet Farm! I'm " + llama.getName() + " !");
-        System.out.println(" You can feed me, give me water, and check my boredom level! But beware! If I get bored, I spit! ");
+        System.out.println("You can feed me, give me water, and check my boredom level! But beware! If I get bored, I spit! ");
+        llama.gettingStarted();
     }
 
 }
