244290dff4d9cace6907b20149a14ffb66307a83
Krystyna Ewing <krystyna.d.l.ewing@gmail.com>
2021-09-17 12:22:03 -0400

    Whoooollllleee lotta changes but it works now

--- src/main/java/virtual_pet/VirtualPet.java
+++ src/main/java/virtual_pet/VirtualPet.java
@@ -21,6 +21,7 @@ public class VirtualPet {
     }
 
     public String getName() {
+
         return name;
     }
 
@@ -39,85 +40,16 @@ public class VirtualPet {
         return boredomLevel;
     }
 
-    public void setHungerLevel(int newHungerLevel) {
+    public void feed(int newHungerLevel) {
         this.hungerLevel = newHungerLevel;
     }
 
-    public void setThirstLevel(int newThirstLevel) {
+    public void water(int newThirstLevel) {
         this.thirstLevel = newThirstLevel;
     }
 
-    public  void setBoredomLevel(int newBoredomLevel) {
+    public  void play(int newBoredomLevel) {
         this.boredomLevel = newBoredomLevel;
-    }
-
-
-
-  public void gettingStarted() {
-        Scanner input =  new Scanner(System.in);
-        String inputNum;
-
-        System.out.println("To get started with my care, what would you like to check first? ");
-        System.out.println("Please enter 1 to check my hunger level");
-        System.out.println("Please enter 2 to check my thirst level");
-        System.out.println("Please enter 3 to check my boredom level");
-        inputNum = input.nextLine();
-
-        if (Integer.parseInt(inputNum) == 1){
-            System.out.println("My hunger level is "  + getHungerLevel());
-        } else if (Integer.parseInt(inputNum) == 2) {
-            System.out.println("My thirst level is "  + getThirstLevel());
-        }else if (Integer.parseInt(inputNum) == 3) {
-            System.out.println("My thirst level is "  + getBoredomLevel());
-        }
-
-  } public void petInteraction(){
-     // Interactions for this class will include:
-     // - feed
-     // - give water
-     // - play
-     // - check time? (for .tick() method)
-        //- stop
-        // Should have set variables and check for them, the same as the gettingStarted() method (might rename it to menu or something)
-     // thinking about doing ths as a while loop so we it can run until the user types in stop
-
-        Scanner interaction =  new Scanner(System.in);
-        String interactionReply;
-
-
-        boolean stop = false ;
-
-
-
-           do {
-               System.out.println("What would you like to do next?");
-               System.out.println("You can check my stats, feed me, give me water, play with me, or make sure I'm not about to spit by");
-               interactionReply = interaction.nextLine();
-
-               if (interactionReply.equals("feed") || interactionReply.equals("feed pet")) {
-                   if (getHungerLevel() >= 100) {
-                       System.out.println("No thank you I'm not hungry");
-                   } else {
-                   setHungerLevel(getHungerLevel() + 10);
-                   System.out.println("Thanks! I was hungry!");
-                   }
-                   } else if (interactionReply.equals("water") || interactionReply.equals("give water")) {
-                   setThirstLevel(getThirstLevel() + 10);
-                   System.out.println("Thanks! I was thirsty");
-                } else if (interactionReply.equals("play") || interactionReply.equals("let's play")) {
-                   setBoredomLevel(getBoredomLevel() + 10);
-                   System.out.println("That was Fun!" );
-               } else if (interactionReply.equals("check stats") || interactionReply.equals("stats")) {
-                   System.out.println("My stats are: ");
-                   System.out.println("Hunger Level: " + getHungerLevel());
-                   System.out.println("Thirst Level: " + getThirstLevel());
-                   System.out.println("Boredom Level: " + getBoredomLevel());
-               }
-           } while (!stop);
-    }
-
-    public void boredomMeter() {
-
     }
 
 
--- src/main/java/virtual_pet/VirtualPetApplication.java
+++ src/main/java/virtual_pet/VirtualPetApplication.java
@@ -1,14 +1,103 @@
 package virtual_pet;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class VirtualPetApplication {
 
-    static VirtualPet llama = new VirtualPet("Llama Charlie", 50,50,50);
+    VirtualPet llama;
+
 
     public static void main(String[] args) {
-        System.out.println("Welcome to the Virtual Pet Farm! I'm " + llama.getName() + " !");
+        VirtualPetApplication game = new VirtualPetApplication();
+        game.llama = new VirtualPet("Llama Charlie", 50,50,50);
+        System.out.println("Welcome to the Virtual Pet Farm! I'm" + game.llama.getName());
         System.out.println("You can feed me, give me water, and check my boredom level! But beware! If I get bored, I spit! ");
-        llama.gettingStarted();
-        llama.petInteraction();
+        game.gettingStarted();
     }
+
+    public void gettingStarted() {
+        Scanner input =  new Scanner(System.in);
+        String inputString;
+
+        System.out.println("My stats are currently: ");
+        System.out.println("You can also type menu for a list of options");
+        inputString = input.nextLine();
+
+        ArrayList<String> menu = new ArrayList<String>();
+        menu.add("check stats");
+        menu.add("interact");
+
+        ArrayList<String> llamaStats = new ArrayList<String>();
+        llamaStats.add("Hunger Level: " + llama.getHungerLevel());
+        llamaStats.add("Thirst Level: " + llama.getThirstLevel());
+        llamaStats.add("Boredom Level: "+ llama.getBoredomLevel());
+
+        if (inputString.equalsIgnoreCase("stats" ) || inputString.equalsIgnoreCase("check stats")) {
+            System.out.println("My stats are currently: \n" + llamaStats );
+        } else if(inputString.equalsIgnoreCase("interact")) {
+            petInteraction();
+        } else if(inputString.equalsIgnoreCase("menu")) {
+            System.out.println("Please choose from the following options: \n" + menu);
+            String menuOptions = "";
+             switch (menuOptions) {
+                 case "check stats":
+                     System.out.println("My stats are: \n" + llamaStats);
+                     break;
+                 case "interaction":
+                     petInteraction();
+
+                 default:
+                     gettingStarted();
+                     break;
+             }
+        } else{
+            System.out.println("It looks like you're having some trouble with figuring out what to do! it's okay");
+            gettingStarted();
+        }
+
+    }
+
+    public void petInteraction(){
+        // Interactions for this class will include:
+        // - feed
+        // - give water
+        // - play
+        // - check time? (for .tick() method)
+        //- stop
+        // Should have set variables and check for them, the same as the gettingStarted() method (might rename it to menu or something)
+        // thinking about doing ths as a while loop so we it can run until the user types in stop
+
+        Scanner interaction =  new Scanner(System.in);
+        String interactionReply;
+
+
+        boolean stop = false ;
+
+
+
+        do {
+            System.out.println("What would you like to do next?");
+            System.out.println("You can check my stats, feed me, give me water, play with me, or make sure I'm not about to spit by");
+            interactionReply = interaction.nextLine();
+
+            if (interactionReply.equalsIgnoreCase("feed") ||interactionReply.equalsIgnoreCase("give food") ){
+                llama.feed(llama.getHungerLevel() + 10);
+                System.out.print("Thanks I was hungry!");
+            } else if(interactionReply.equalsIgnoreCase("water") || interactionReply.equalsIgnoreCase("give water")) {
+                llama.water(llama.getThirstLevel() - 10);
+                System.out.println("Thanks! I was getting really thirsty!");
+            } else if( interactionReply.equalsIgnoreCase("play")|| interactionReply.equalsIgnoreCase("let's play")) {
+                llama.play(llama.getBoredomLevel() - 10);
+                System.out.print("Thanks! that was fun!");
+
+            }
+        } while (!stop);
+    }
+
+    public void boredomMeter() {
+
+    }
+
 
 }
